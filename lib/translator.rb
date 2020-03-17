8f7ffb6bd9cfd885ab1d16b2b7110b56c885747a
Blake <blakewoodruff4@gmail.com>
2020-03-17 20:14:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,16 +8,16 @@ def load_library(file_path)
   hash= {"get_meaning" => {}, "get_emoticon" => {}}
   
   library.each{|meaning,emoticons|
-    hash[:get_meaning][emoticons[1]]=meaning
-    hash[:get_emoticon][emoticons[0]]=emoticons[1]
+    hash["get_meaning"][emoticons[1]]=meaning
+    hash["get_emoticon"][emoticons[0]]=emoticons[1]
   }
   hash
 end
 
 def get_japanese_emoticon(file_path,emoticon)
   library = load_library(file_path)
-  if library[:get_emoticon].include?(emoticon)
-    libary[:get_emoticon][emoticon]
+  if library["get_emoticon"].include?(emoticon)
+    libary["get_emoticon"][emoticon]
   else
     "Sorry, emoticon not found in library."
   end
@@ -25,8 +25,8 @@ end
 
 def get_english_meaning(file_path,emoticon)
   library = load_library(file_path)
-  if library[:get_meaning].include?(emoticon)
-    libary[:get_meaning][emoticon]
+  if library["get_meaning"].include?(emoticon)
+    libary["get_meaning"][emoticon]
   else
     "Sorry, emoticon not found in library."
   end
